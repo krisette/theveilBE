7dcb9647b83b6447c2ff649058efc5239f2de300
Krisette Odegard <kmodeg@gmail.com>
2023-10-08 15:16:14 -0700

    fix: added logic back in for 1 card draw

--- utils/drawCardsLogic.js
+++ utils/drawCardsLogic.js
@@ -11,13 +11,30 @@ const drawCardsLogic = async (count) => {
       throw new Error(`Invalid max index: ${maxIndex}`);
     }
 
-    const randomIndices = await getRandomNumber(count, 0, maxIndex);
-    const randomOrientations = await getRandomNumber(count, 0, 1);
+    const drawnCards = [];
+    const promises = [];
 
-    const drawnCards = randomIndices.map((index, i) => ({
-      ...cards[index],
-      orientation: randomOrientations[i] === 0 ? 'up' : 'reversed',
-    }));
+    for (let i = 0; i < count; i += 1) {
+      const randomIndexPromise = getRandomNumber(1, 0, maxIndex);
+      const randomOrientationPromise = getRandomNumber(1, 0, 1);
+
+      promises.push(randomIndexPromise);
+      promises.push(randomOrientationPromise);
+    }
+
+    const results = await Promise.all(promises);
+
+    for (let i = 0; i < count; i += 1) {
+      const randomIndex = results[i * 2];
+      const randomOrientation = results[i * 2 + 1];
+
+      const drawnCard = {
+        ...cards[randomIndex],
+        orientation: randomOrientation === 0 ? 'up' : 'reversed',
+      };
+
+      drawnCards.push(drawnCard);
+    }
 
     return drawnCards;
   } catch (error) {
